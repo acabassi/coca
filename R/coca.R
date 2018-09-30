a6ca2d85f3f35b1f00438dea4ee28988b73bc726
Alessandra Cabassi <ac2051@cam.ac.uk>
2018-09-30 11:51:44 +0100

    make notation consistent
    “MOC” instead of “data”

--- R/coca.R
+++ R/coca.R
@@ -3,7 +3,7 @@
 #' This function allows to do Cluster-Of-Clusters-Analysis on a binary matrix where each column is a clustering of the data,
 #' each row corresponds to a data point and the element in position (i,j) is equal to 1 if data point i belongs to cluster
 #' j, 0 otherwise.
-#' @param data N X C data matrix, where C is the total number of clusters considered.
+#' @param MOC N X C data matrix, where C is the total number of clusters considered.
 #' @param K number of clusters.
 #' @param B number of iterations of the Consensus Cluster step.
 #' @param pItem proportion of items sampled at each iteration of the Consensus Cluster step.
@@ -17,13 +17,13 @@
 #' 487(7407), pp.61–70.
 #' @export
 
-coca = function(dataset, K, B = 1000, pItem = 0.8, hclustMethod = 'average'){
+coca = function(MOC, K, B = 1000, pItem = 0.8, hclustMethod = 'average'){
 
 
   output = list()
 
   # Step 1. Compute the consensus matrix
-  output$consensusMatrix <- consensusCluster(dataset, K, B, pItem)
+  output$consensusMatrix <- consensusCluster(MOC, K, B, pItem)
 
   # Step 2. Use hierarchical clustering on the consensus matrix to find the clustering
   distances <- as.dist(1 - output$consensusMatrix)
