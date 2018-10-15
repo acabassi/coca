b19061ff0e8a76f59e74632a870ed72087ce2be8
Alessandra Cabassi <ac2051@cam.ac.uk>
2018-10-15 15:47:21 +0100

    add option to save plot to file

--- R/plot-moc.R
+++ R/plot-moc.R
@@ -11,38 +11,46 @@
 #' number of different annotations.
 #' @param clr Cluster rows. Default is FALSE.
 #' @param clc Cluster columns. Default is FALSE.
+#' @param save Boolean. If TRUE, plot is saved as a pdf file.
 #' @param fileName File name. Default is "moc.pdf".
 #' @author Alessandra Cabassi \email{ac2051@cam.ac.uk}
 #' @references The Cancer Genome Atlas, 2012. Comprehensive molecular portraits of human breast
-#' tumours. Nature,
-#' 487(7407), pp.61–70.
+#' tumours. Nature, 487(7407), pp.61–70.
 #' @export
 #'
 plotMOC = function(moc, datasetIndicator, datasetNames, annotations = NA,
-                   clr = FALSE, clc = FALSE, fileName = "moc.pdf"){
+                   clr = FALSE, clc = FALSE, save = FALSE, fileName = "moc.pdf"){
 
   moc = t(moc)
+
+  # Get number of datasets
   M = length(table(datasetNames))
+
+  # Get sum of the number of clusters in each dataset
   sumK = dim(moc)[1]
+
+  # If dataset names are not provided
   if(is.null(datasetNames)){
-    newDatasetNames = as.string(datasetIndicator)
-  }else{
-    for(i in 1:sumK){
-      nClustersLeft <- sum(datasetNames == datasetNames[i])
-      if(nClustersLeft>1){
-        datasetNames[i] = paste(datasetNames[i], nClustersLeft, sep = " ")
-      }
-    }
+    # Dataset names = dataset indicators
+    datasetNames = as.string(datasetIndicator)
   }
 
+  # Make dataset names unique by adding a different number at the end of
+  # the dataset name for each cluster
+  for(i in 1:sumK){
+    nClustersLeft <- sum(datasetNames == datasetNames[i])
+      datasetNames[i] = paste(datasetNames[i], nClustersLeft, sep = " ")
+  }
 
+  # We want every dataset to have a different colour :)
   for(i in 1:sumK){
     moc[i,] <- moc[i,]*datasetIndicator[i]
   }
 
-  rownames(moc) <- datasetNames
 
-  pdf(fileName, width = 10, height = 5)
+  # Plot!
+  rownames(moc) <- datasetNames
+  if(save) pdf(fileName, width = 10, height = 5)
   pheatmap::pheatmap(moc,  legend = TRUE,
            legend_breaks = 0:M,
           # legend_labels = c("0", table(datasetNames)),
@@ -51,6 +59,10 @@ plotMOC = function(moc, datasetIndicator, datasetNames, annotations = NA,
            cluster_cols = clc, clustering_distance_cols = "binary",
            annotation_col = annotations)
 
-  dev.off()
+  if(save){
+    dev.off()
+    warning('After saving a pheatmap plot to file, you sometimes have to repeat the `dev.off()` command
+            in order to shut down the plotting device completely.')
+  }
 
 }
