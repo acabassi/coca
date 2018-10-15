97b951384f813071ddf49140e637c6a6bb222c99
Alessandra Cabassi <ac2051@cam.ac.uk>
2018-10-15 15:44:34 +0100

    fix bug + add comments

--- R/consensus-cluster.R
+++ R/consensus-cluster.R
@@ -24,25 +24,41 @@
 
 consensusCluster = function(data, K, B = 100, pItem = 0.8, clMethod = "km", distHC = "euclidean"){
 
+  # Save number of observations
   N <- dim(data)[1]
+  # Create vector of data indices that will be used to update the co-clustering matrix
   dataIndices <- c(1:N)
+  # Initialise empty co-clustering matrix and an auxiliar
   coClusteringMatrix <- indicatorMatrix <- matrix(0, N, N)
 
+  # For each step of the algorithm
   for(b in 1:B){
-    items <- sample(N, ceiling(N*0.8), replace = FALSE)
+    # Sample a proportion pItem of the observations without replacement
+    items <- sample(N, ceiling(N*pItem), replace = FALSE)
 
+    # If the chosen clustering method is k-means
     if(clMethod == "km")
+      # Apply k-means to the subsample and extract cluster labels
       cl <- stats::kmeans(data[items,], K, iter.max = 100)$cluster
+
+    # If the chosen clustering method is hierarchical clustering
     else if(clMethod == "hc"){
+      # Calculate pairwise distances between observations
       distances <- stats::dist(data, method = distHC)
+      # Apply hierarchical clustering to the subsample
       hClustering <- stats::hclust(distances, method = "average")
       clLabels <- stats::cutree(hClustering, K)
     }
 
+    # Update matrix containing counts of number of times each pair has been sampled together
     indicatorMatrix <- indicatorMatrix + crossprod(t(as.numeric(dataIndices%in%items)))
+    # For each cluster
     for(k in 1:K){
+      # Update counts of number of times each pair has been put in the same cluster
       coClusteringMatrix[items,items] <- coClusteringMatrix[items,items] + crossprod(t(as.numeric(cl==k)))
     }
   }
+
+  # Normalise co-clustering matrix
   coClusteringMatrix/indicatorMatrix
 }
