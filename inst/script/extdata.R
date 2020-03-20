73f06dee2d2635ea9692c4b9ca91129a1875caee
Alessandra Cabassi <ac2051@cam.ac.uk>
2020-03-20 18:26:33 +0000

    fix vignette

--- inst/script/extdata.R
+++ inst/script/extdata.R
@@ -7,9 +7,9 @@ true_labels <- c(rep(1, n_elements_per_cluster),
                  rep(4, n_elements_per_cluster),
                  rep(5, n_elements_per_cluster))
 
-dataset1 <- t(mvtnorm::rmvnorm(2, true_labels))
-dataset2 <- t(mvtnorm::rmvnorm(2, true_labels))
-dataset3 <- t(mvtnorm::rmvnorm(2, true_labels))
+dataset1 <- t(mvtnorm::rmvnorm(2, true_labels*3))
+dataset2 <- t(mvtnorm::rmvnorm(2, true_labels*3))
+dataset3 <- t(mvtnorm::rmvnorm(2, true_labels*3))
 
 write.csv(dataset1, "inst/extdata/dataset1.csv")
 write.csv(dataset2, "inst/extdata/dataset2.csv")
